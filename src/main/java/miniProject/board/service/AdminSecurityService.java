28a5b72089c18fc00fe6e976d1b9e72c3f2c43cc
chaanheeLEE <chanhee1519@gmail.com>
2024-03-22 16:30:56 +0900

    feat: admin login with spring-security

--- src/main/java/miniProject/board/service/AdminSecurityService.java
+++ src/main/java/miniProject/board/service/AdminSecurityService.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import miniProject.board.entity.Admin;
 import miniProject.board.repository.AdminRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -25,9 +29,8 @@ public class AdminSecurityService implements UserDetailsService {
             throw new UsernameNotFoundException("계정을 찾을 수 없습니다.");
         }
         Admin admin = _admin.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
 
-        return User.withUsername(admin.getName())
-                .password(admin.getPassword())
-                .build();
+        return new User(admin.getUsername(), admin.getPassword(), authorities);
     }
 }
--- src/main/java/miniProject/board/service/AdminService.java
+++ src/main/java/miniProject/board/service/AdminService.java
@@ -4,15 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import miniProject.board.entity.Admin;
 import miniProject.board.repository.AdminRepository;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,12 +15,11 @@ public class AdminService {
     private final AdminRepository adminRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Admin create(String name, String password){
+    public void create(String name, String password){
         Admin admin = new Admin();
         admin.setName(name);
         admin.setPassword(passwordEncoder.encode(password));
         this.adminRepository.save(admin);
-        return admin;
     }
 
     public boolean isEmpty() {
